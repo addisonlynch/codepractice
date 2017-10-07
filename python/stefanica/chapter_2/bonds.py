a87654aca8f788115b15fece6204c0626268f574
addisonlynch <addisonlynch@ucla.edu>
2017-10-07 01:18:25 +0000

    Initial Commit

--- python/stefanica/chapter_2/bonds.py
+++ python/stefanica/chapter_2/bonds.py
@@ -9,6 +9,7 @@ def integrate_definite(f, a, b):
     return quad(f, a, b)[0]
 
 
+f  = lambda x : 0.045 + (0.005*(1+x)*math.log(1+x))/x
 
 
 #auxillary zero rate curve function
@@ -23,30 +24,53 @@ def rate_curve(x):
 
 
 
+#function,face_value coupon, duration, intervals, discreet
+def bond_price_instant(f,c,j,n,d,fx):
+    h=j/n
 
-def bond_price_instant(f,c,n):
+    def calculateIntervals(length,n_intervals):
+        intervals=[]
+        h = float(length)/n_intervals
+        htemp = h
+        while htemp <= length:
+            intervals.append((float(htemp-h),float(htemp)))
+            htemp += h
+        return intervals
 
-
-#calculate the price of a semi-annual coupon bond given zero rate curve
-def bond_price_sa(f, c, n):
-
-    #compute the discount factor
-    price =0
-    def stepUp(i, j, s):
+    interval_list = calculateIntervals(j,n)
+    #print(calculateIntervals(2,4)[0][0])
+    
+    def stepUp(i,j,s):
         while i<j:
             yield i
             i += s
 
-    for i in stepUp(0.5,2,0.5):
-        price += (c/2)*(f*math.exp((-1)*rate_curve(i)*i))
-    price+=(f+(c/2)*f)*(math.exp((-1)*rate_curve(2.0)*(2.0)))
+    def discount_factor(start,end,discr,fx):
+        if discr:
+            ans = rate_curve(end)
+        else:
+            ans =quad(fx,start,end)[0]        
+        retval= math.exp((-1)*ans*(end))
+        print("Discount factor for " + str(start)+ " to " + str(end)+ ": " + str(retval))
+        return retval
+    price = float(0)
+    for inter in interval_list:
+        if inter[0] == j-h:
+            price += (f+(c/2)*f)*discount_factor(inter[0], inter[1],d,fx)
+            break
+        discount =discount_factor(inter[0],inter[1],d,fx)
+        price += (c/2)*f*discount
+    
+   
     return price
+    
 
-
+#calculate the price of a semi-annual coupon bond given zero rate curve
 
 
 def main():
-    p =bond_price_sa(100, 0.05, 4)
+
+    p =bond_price_instant(100, 0.05, 2, 4, True, "None")
     print(p)
 
 if __name__ == "__main__":
